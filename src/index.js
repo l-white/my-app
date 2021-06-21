1c07665ee02a2508710c7244c0e7d0e8368bc8da
White <whitel1@milwaukee.k12.wi.us>
2021-06-21 10:50:24 -0500

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: "Laura",
+  lastName: "White"
+}
+
+const userImage = <img src="https://0.gravatar.com/avatar/032eadee734869888f5120264c289713?s=400&d=mm" />
+
+const element = (
+  <div>
+    <h1>Hello, {formatName(user)}!</h1>
+    <p>Good to see you here.</p>
+    <img src="https://0.gravatar.com/avatar/032eadee734869888f5120264c289713?s=400&d=mm" width="200"/>
+  </div>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
   </React.StrictMode>,
+  */
+ element,
   document.getElementById('root')
 );
 

4aa6ed98e1c829e49b728566eb36733ffc9b5180
White <whitel1@milwaukee.k12.wi.us>
2021-06-22 11:16:05 -0500

    modified Header component

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,6 @@ function Header(){
     return(
         <header>
             <h1><a href="http://reactjs.org/">React</a></h1>
-            <p>Check out these React resources:</p>
         </header>
     );
 }
